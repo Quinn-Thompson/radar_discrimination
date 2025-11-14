0a610fc34492fb1706d1391de9cd6a70cea47979
Quinn-Thompson <loopvertical@gmail.com>
2025-11-14 12:33:42 -0800

    (NOT WORKING) forgot to stage helpers

--- gui_backend/helpers.py
+++ gui_backend/helpers.py
@@ -1,3 +1,14 @@
 """A helper function to prevent circular imports."""
+from gui.sub_widgets.signal_window import ChirpValues, LoopValues, DelayValues
+from typing import Optional
+from ifxradarsdk.fmcw.types import FmcwElementType
 
 _RECEIVER_COUNT = 3
+
+class ElementSequence():
+    def __init__(self):
+        self.next_element: "ElementSequence" = None
+        self.type: FmcwElementType = None
+        self.delay: Optional[DelayValues]
+        self.chirp: Optional[ChirpValues]
+        self.loop: Optional[LoopValues]
